a68f1bde8281f84e6ce2aae5858329654a979f27
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-02-28 16:15:59 -0700

    Outputting Conditional Content

--- react 1/src/App.js
+++ react 1/src/App.js
@@ -30,7 +30,7 @@ const App = () => {
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
-      return [expense, ...expenses];
+      return [expense, ...prevExpenses];
     });
   };
 
