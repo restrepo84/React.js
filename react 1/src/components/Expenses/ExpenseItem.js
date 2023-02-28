b7a94189de39075bf03bdb0f32f940728913ea59
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-02-27 17:14:27 -0700

    Assignment 2 Working with events & stateless vs stateful components

--- react 1/src/components/Expenses/ExpenseItem.js
+++ react 1/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 // useState is a function provided by the React Library this function allows us to to define values as state where changes to these values should reflect in the component function being called again
 // useState is also called React hook,,, beside this one there's also other hooks
 
@@ -7,21 +7,13 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Update!");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
